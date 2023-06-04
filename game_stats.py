914112be87789bead9d07f60f199100ed9eb116a
hhzhao <zhh_0500050533@163.com>
2023-06-04 20:08:24 +0800

    首次提交

---
+++ game_stats.py
@@ -0,0 +1,12 @@
+class GameStats:
+    """跟踪游戏的统计信息"""
+
+    def __init__(self, ai_game):
+        """初始化统计信息"""
+        self.ships_left = None
+        self.settings = ai_game.settings
+        self.reset_stats()
+        self.game_active = True
+
+    def reset_stats(self):
+        self.ships_left = self.settings.ship_limit
